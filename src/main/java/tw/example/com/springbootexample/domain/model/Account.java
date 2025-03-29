e1bc3db67ec01514304b8a7df9667698070618b4
ray <ray9793.pri@gmail.com>
2025-03-29 15:59:55 +0800

    class Account to implements UserDetails for spring security

--- src/main/java/tw/example/com/springbootexample/domain/model/Account.java
+++ src/main/java/tw/example/com/springbootexample/domain/model/Account.java
@@ -1,45 +1,82 @@
-// package tw.example.com.springbootexample.domain.model;
+package tw.example.com.springbootexample.domain.model;
 
-// import java.util.Collection;
+import java.util.Collection;
+import java.util.List;
 
-// import org.springframework.security.core.GrantedAuthority;
-// import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-// public class Account implements UserDetails {
 
-//     @Override
-//     public Collection<? extends GrantedAuthority> getAuthorities() {
-//         throw new UnsupportedOperationException("Unimplemented method 'getAuthorities'");
-//     }
+public class Account implements UserDetails {
+    // @Autowired
+    // private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 
-//     @Override
-//     public String getPassword() {
-//         throw new UnsupportedOperationException("Unimplemented method 'getPassword'");
-//     }
+    private Long Id;
+    private String userName;
+    private String password;
+    private AccountRole role;
 
-//     @Override
-//     public String getUsername() {
-//         throw new UnsupportedOperationException("Unimplemented method 'getUsername'");
-//     }
+    public Account(String userName, String password, AccountRole role) {
+        this.userName = userName;
+        this.password = password;
+        this.role = role;
+    }
 
-//     @Override
-//     public boolean isAccountNonExpired() {
-//         throw new UnsupportedOperationException("Unimplemented method 'isAccountNonExpired'");
-//     }
+    /**
+     * 用於提供該用戶授權權限集合
+     * @return 用戶被授權的權限集合
+     */
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
 
-//     @Override
-//     public boolean isAccountNonLocked() {
-//         throw new UnsupportedOperationException("Unimplemented method 'isAccountNonLocked'");
-//     }
+    @Override
+    public String getUsername() {
+        return userName;
+    }
 
-//     @Override
-//     public boolean isCredentialsNonExpired() {
-//         throw new UnsupportedOperationException("Unimplemented method 'isCredentialsNonExpired'");
-//     }
+    @Override
+    public String getPassword() {
+        return password;
+    }
 
-//     @Override
-//     public boolean isEnabled() {
-//         throw new UnsupportedOperationException("Unimplemented method 'isEnabled'");
-//     }
-    
-// }
+    /**
+     * 用來判斷使用者的帳戶是否過期
+     * @return 如果帳戶已過期，返回false，表示使用者不應該被授權，反之則true。
+     */
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    /**
+     * 用來判斷使用者的帳戶是否被鎖定
+     * @return 如果帳戶被鎖定，返回false，表示使用者不應該被授權。
+     */
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    /**
+     * 用來判斷使用者的認證信息是否過期，例如密碼是否過期
+     * @return 如果認證信息已過期，返回false，表示使用者不應該被授權。
+     */
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    /**
+     * 用來判斷使用者是否啟用，如果使用者已被禁用
+     * @return 返回false，表示使用者不應該被授權。
+     */
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/tw/example/com/springbootexample/domain/model/AccountRole.java
@@ -0,0 +1,7 @@
+package tw.example.com.springbootexample.domain.model;
+
+public enum AccountRole {
+    Programmer,
+    BusinessAnalyst,
+    SystemAdmin
+}
